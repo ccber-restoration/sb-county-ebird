022eb389c063fb61c3d08ec6dea7db35a72d21a3
fjoyce <fhjoyce@ucsb.edu>
2026-02-13 16:42:59 -0800

    streamline script a bit, add section for campus lagoon hotspot data

--- code/auk_quickstart.R
+++ code/auk_quickstart.R
@@ -6,6 +6,7 @@
 #note that the CRAN version of auk is not up to date with the 2025 taxonomy, so I installed the development version from github
 #remotes::install_github("CornellLabofOrnithology/auk")
 
+#load packages ----
 library(auk)
 library(dplyr)
 library(ggplot2)
@@ -16,6 +17,11 @@ library(sf)
 
 #check auk version
 auk_version()
+
+#load ebird_taxonomy (from within package)
+data("ebird_taxonomy")
+
+# filepaths and checklist metadata ----
 
 #define filepath to sampling event data
 f_sed <- "ebd_US-CA-083_smp_relDec-2025/ebd_US-CA-083_smp_relDec-2025_sampling.txt"
@@ -31,8 +37,13 @@ ncos_checklists <- checklists %>%
 
 #5435 checklists, as of December 2025
 
+#filter to just Campus Lagoon checklists
+cl_checklists <- checklists %>% 
+  filter(locality == "UCSB--Campus Lagoon")
 
-#define filepath
+#2,852 checklists
+
+#define filepath to actual survey data ----
 f_in <- "ebd_US-CA-083_smp_relDec-2025/ebd_US-CA-083_smp_relDec-2025.txt"
 
 #define output filepath (after filtering)
@@ -43,6 +54,7 @@ f_out <- "ebd_filtered_south_coast.txt"
 #format is left longitude, bottom latitude, right longitude, top latitude
 south_coast_bbox <- c(-120.2, 34.3, -119.4, 34.45)
 
+#load ebd within bounding box for the south coast
 ebird_data <- f_in |> 
   # 1. reference file
   auk_ebd() |> 
@@ -57,12 +69,13 @@ ebird_data <- f_in |>
   #do not "roll up" species
   read_ebd(rollup = FALSE)
 
+# 2M observations
 
+#filter to NCOS ----
 ncos_data <- ebird_data %>% 
   filter(locality == "UCSB North Campus Open Space (formerly Ocean Meadows Golf Course)")
 
 write_rds(ncos_data, "data-processed/ncos_data.rds")
-
 
 ncos_species_df <- ncos_data %>% 
   #when there is subspecies, make the common name the subspecies_common name
@@ -75,9 +88,6 @@ ncos_full_list <- ncos_species_df %>%
   select(new_common_name) %>% 
   unique()
   
-#load ebird_taxonomy (from within package)
-data("ebird_taxonomy")
-
 ncos_ebird_taxonomy <- ncos_full_list %>% 
   inner_join(ebird_taxonomy, by = join_by("new_common_name" == "common_name")) %>% 
   #sort by taxonomy
@@ -85,4 +95,30 @@ ncos_ebird_taxonomy <- ncos_full_list %>%
 
 write_csv(ncos_ebird_taxonomy, file = "data-processed/ncos_ebird_list.csv")
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ----
 
+#filter to Campus Point ----
+cl_data <- ebird_data %>% 
+  filter(locality == "UCSB--Campus Lagoon")
+
+#77,844 observations
+
+write_rds(cl_data, "data-processed/cl_data.rds")
+
+cl_species_df <- cl_data %>% 
+  #when there is subspecies, make the common name the subspecies_common name
+  mutate(new_common_name = case_when(
+    !is.na(subspecies_common_name) ~ subspecies_common_name,
+    .default = common_name
+  ))
+
+cl_full_list <- cl_species_df %>% 
+  select(new_common_name) %>% 
+  unique()
+
+cl_ebird_taxonomy <- cl_full_list %>% 
+  inner_join(ebird_taxonomy, by = join_by("new_common_name" == "common_name")) %>% 
+  #sort by taxonomy
+  arrange(taxonomic_order)
+
+write_csv(cl_ebird_taxonomy, file = "data-processed/cl_ebird_list.csv")
